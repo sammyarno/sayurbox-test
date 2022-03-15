952ed676bf1266a074891e39ef6a87006d07b6c1
Samuel Saputra <samuel.saputra@modalrakyat.id>
2022-03-16 00:09:08 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+
+import cache from './utils/cache';
+import App from './containers/App';
+import './styles/index.scss';
+
+const client = new ApolloClient({
+  cache,
+  uri: process.env.REACT_APP_GRAPHQL_URL,
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
